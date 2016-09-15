080a21e14eba70a41ae4132c654e62258c5729fc
Mahati Gollamudi <github email address>
2016-09-15 16:15:48 -0400

    Alter Serialize action in Controller, change routes, update show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
@@ -5,5 +6,4 @@ Rails.application.routes.draw do
   resources :products, only: [:show, :index, :new, :create]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/data', to: 'products#data'
 end
